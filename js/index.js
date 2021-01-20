74ad557d5119f396441bd710098010336e7fcc27
diego <diego@laptop-swift3.com>
2021-01-20 01:03:37 -0300

    cambios en proyectos

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
+const proyects = document.getElementsByClassName("proyect");
 
-function  porsentajedeScroll() {
-    return (100 * window.scrollY)/(document.body.scrollHeight - screen.height);
-}
+//cargar svg
+document.addEventListener('DOMContentLoaded',()=>{
+    const svgs = [...document.querySelectorAll('svg[data-src]')];
+    svgs.forEach(svg =>{
+        fetch(svg.dataset.src)
+            .then(data=>data.text())
+            .then(xml=>{
+                svg.innerHTML = xml;
+                const viewBox = svg.querySelector('svg').getAttribute('viewBox');
+                svg.setAttribute('viewBox',viewBox)
+            })
+    })
+})
+
 function activarMenu(){
     let items = document.getElementsByClassName("item");
     for(itm of items){
@@ -20,6 +32,20 @@ for(itemLink of itemsLinks){
     itemLink.addEventListener("click", function(evt){activarMenu()});
 }
 
+const arrayProyects = [...proyects];
+arrayProyects.forEach(proyect =>{
+    const video = proyect.getElementsByTagName('video')[0];
+    if(video != null){
+    proyect.addEventListener('mouseover',event=>{
+        if(video.paused || video.ended || video.seeking || video.readyState < video.HAVE_FUTURE_DATA){
+            video.play().then().catch()
+        }
+    })
+    proyect.addEventListener('mouseout',event=>{
+        video.pause()
+    })
+    }
+});
 //window.addEventListener('scroll', function(evt){
 
 
